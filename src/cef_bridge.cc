6be792592b49ebde473aafb28c8b6c804015aa12
Damian Tarnawski <gthetarnav@gmail.com>
2026-03-28 14:36:20 +0100

    Try diabling gpu.

--- src/cef_bridge.cc
+++ src/cef_bridge.cc
@@ -259,6 +259,12 @@ private:
 
 class Bridge_App final : public CefApp, public CefRenderProcessHandler {
 public:
+	void OnBeforeCommandLineProcessing(const CefString& process_type, CefRefPtr<CefCommandLine> command_line) override {
+		command_line->AppendSwitch("disable-gpu");
+		command_line->AppendSwitch("disable-gpu-compositing");
+		command_line->AppendSwitch("disable-software-rasterizer");
+	}
+
 	CefRefPtr<CefRenderProcessHandler> GetRenderProcessHandler() override {
 		return this;
 	}
